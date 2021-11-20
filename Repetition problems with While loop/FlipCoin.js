8e179715809e39b013627a331ccaf690dbf03dc0
unknown <shaunak12wankhade@ggmail.com>
2021-11-20 16:09:03 +0530

    [Shaunak] Added : Flip Coin problems to get heads or tails 11 times

---
+++ Repetition problems with While loop/FlipCoin.js
@@ -0,0 +1,24 @@
+var headCount=0;
+var tailCount=0;
+while(headCount<=10 || tailCount<=10)
+{
+    var flip=Math.floor(Math.random()*10) % 2;
+    if(flip==0)
+    {
+        console.log("Head")
+        headCount++;
+    }
+    else
+    {
+        console.log("Tail")
+        tailCount++;
+    }
+}
+if(headCount==11)
+{
+    console.log("Head is the Winner");
+}
+else
+{
+    console.log("Tail is the Winner");
+}
